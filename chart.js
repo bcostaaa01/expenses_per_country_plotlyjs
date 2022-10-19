627c736db5e071ff9199f15fa2b68b7d11e21bbb
Bruno Miguel Alves Costa <brunomiguelalvescosta@Bruno-Costas-MacBook.local>
2022-10-19 21:18:42 +0100

    Add new data: new countries and properties

--- chart.js
+++ chart.js
@@ -2,7 +2,7 @@ data = [
   {
     country: "Portugal",
     type: "scatterpolar",
-    r: [730, 70, 20, 2.0, 300, 20],
+    r: [730, 70, 20, 1.9, 300, 20, 800],
     theta: [
       "Minimum Wage",
       "Electricity",
@@ -10,13 +10,29 @@ data = [
       "Gas",
       "Groceries",
       "Insurance",
+      "Avg. rent",
     ],
     fill: "toself",
   },
   {
     country: "Netherlands",
     type: "scatterpolar",
-    r: [1670, 150, 100, 2.0, 400, 109],
+    r: [1670, 150, 100, 2.0, 400, 109, 850],
+    theta: [
+      "Minimum Wage",
+      "Electricity",
+      "Water",
+      "Gas",
+      "Groceries",
+      "Insurance",
+      "Avg. rent",
+    ],
+    fill: "toself",
+  },
+  {
+    country: "Spain",
+    type: "scatterpolar",
+    r: [1200, 70, 20, 1.6, 300, 20, 500],
     theta: [
       "Minimum Wage",
       "Electricity",
@@ -28,9 +44,9 @@ data = [
     fill: "toself",
   },
   {
-    country: "Spain",
+    name: "Germany",
     type: "scatterpolar",
-    r: [1200, 70, 20, 1.6, 300, 20],
+    r: [2500, 150, 30, 1.9, 300, 180, 700],
     theta: [
       "Minimum Wage",
       "Electricity",
@@ -38,6 +54,7 @@ data = [
       "Gas",
       "Groceries",
       "Insurance",
+      "Avg. rent",
     ],
     fill: "toself",
   },
@@ -47,7 +64,7 @@ layout = {
   polar: {
     radialaxis: {
       visible: true,
-      range: [0, 1670],
+      range: [0, 2500],
     },
   },
   showlegend: false,
